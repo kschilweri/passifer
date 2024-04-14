5896d73dcac77a656f2e58068c353ba6824b98c9
Kaustubh Chilweri <kschilweri28@gmail.com>
2024-04-13 19:12:40 -0700

    user not allowed to go to addpassword page without logging in

--- src/components/AddPassword.jsx
+++ src/components/AddPassword.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const AddPassword = () => {
-  return (
-    <div>
-i am remodelled page
-    </div>
-  )
-}
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("email") && localStorage.getItem("uuid")) {
+      navigate("/addpassword");
+    } else {
+      navigate("/");
+    }
+  }, []);
+
+  return <div>i am remodelled page</div>;
+};
 
 export default AddPassword;
-

c52d7aa3b4fc4fff7c6c91eb7dc8100dc97341e3
Kaustubh Chilweri <kschilweri28@gmail.com>
2024-04-14 16:10:32 -0700

    small fixes

--- src/components/AddPassword.jsx
+++ src/components/AddPassword.jsx
@@ -12,7 +12,80 @@ const AddPassword = () => {
     }
   }, []);
 
-  return <div>i am remodelled page</div>;
+  return (
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_20%,#63e_100%)]">
+      <div className="flex justify-center items-center mt-52">
+        <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+          <div class="absolute inset-0 bg-gradient-to-r from-blue-800 to-purple-700 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+          <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+            <div class="max-w-md mx-auto ">
+              <div>
+                <h1 class="text-2xl font-semibold">
+                  Add Password
+                </h1>
+              </div>
+              <div class="divide-y divide-gray-200">
+                <div class="py-8  text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                  <div class="relative">
+                    <input
+                      autocomplete="off"
+                      id="app"
+                      name="app"
+                      type="text"
+                      class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                      placeholder="App Name"
+                    />
+                    <label
+                      for="app"
+                      class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                    >
+                      App Name
+                    </label>
+                  </div>
+                  <div class="relative">
+                    <input
+                      autocomplete="off"
+                      id="email"
+                      name="email"
+                      type="text"
+                      class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                      placeholder="Email address"
+                    />
+                    <label
+                      for="email"
+                      class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                    >
+                      Email Address/Username
+                    </label>
+                  </div>
+                  <div class="relative">
+                    <input
+                      autocomplete="off"
+                      id="password"
+                      name="password"
+                      type="text"
+                      class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                      placeholder="Password"
+                    />
+                    <label
+                      for="password"
+                      class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                    >
+                      Password
+                    </label>
+                  </div>
+                  <div className="flex py-6 justify-center">
+                  <div class="button-borders">
+                    <button class="primary-button">SAVE</button>
+                  </div></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddPassword;
